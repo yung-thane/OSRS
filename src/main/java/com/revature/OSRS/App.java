ccaef001a1bd5b821572811bcfa7023a039ad7ae
Ronald <ronald.cristobal.lemus@outlook.com>
2022-03-06 12:45:58 -0500

    Working Tomcat and /items mapping and ?name= 1:08 left on 2/24

--- src/main/java/com/revature/OSRS/App.java
+++ src/main/java/com/revature/OSRS/App.java
@@ -6,6 +6,12 @@ import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -23,60 +29,25 @@ import java.util.Objects;
 
 public class App {
     public static void main(String[] args){
-        //Loads Data
-        List<Item> items = loadItems();
-
-        //Search query
-        String searchQuery = "Dragon harpoon";
-        System.out.println(searchByName(searchQuery, items));
-
+        AppContext.build();
+        //Serve on Tomcat server
+        Tomcat server = new Tomcat();
+        server.getConnector();
+        server.addContext("", null);
+        //Using an anonymous class, HttpServlet
+        server.addServlet("", "itemServlet", new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                String name = req.getParameter("name");
+                String result = AppContext.searchByName(name);
+                resp.getWriter().println(result);
+            }
+        }).addMapping("/items");
+        try {
+            server.start();
+        } catch (LifecycleException e) {
+            System.err.println("Embedded server failed to start.");
+        }
         //Save results
-
         }
-        public static BufferedReader loadItemCSV(String csvFile) {
-            if (!csvFile.endsWith(".csv"))
-                throw new IllegalArgumentException("Must be a CSV file.");
-            //Uses class.ClassLoader, .getResource, and .to.URI to scan through folders and get OSRSItems.csv as a URI named uri.
-            //It's useful because once program is packaged and shipped the folder structure changes making hard coding a filepath
-            // problematic. Take care to note that once you load ClassLoader, any changes made to the file will only be reflected
-            // once the program/ClassLoader is restarted. Surround with try/catch to handle URISyntaxException.
-            try {
-                URI uri = Objects.requireNonNull(App.class.getClassLoader().getResource(csvFile)).toURI();
-                //Uses a Paths.get to save uri as a Path named filepath.
-                Path filepath = Paths.get(Objects.requireNonNull(uri));
-                return Files.newBufferedReader(filepath);
-            } catch (URISyntaxException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                System.err.println("Couldn't load file.");
-            }
-        }
-
-
-
-        public static List<Item> parseItems(BufferedReader br) throws IOException, CsvException {
-            List<Item> items = new ArrayList<>();
-            //Using OpenCSV library implementation to build a parser using CSVParserBuilder, .withSeparator to separate columns with commas as a delimeter,
-            //.skipline(1) to skip the header in the parsing, and .build to complete the build.
-            //CSVreader named reader is built using CSVReaderBuilder with the br BufferedReader passed as an argument and withCSVParser with parser passed as an argument
-            //and .build to complete the build. We then use the CSVReader we just made to save the lines of information to a List<String[]> named lines.
-            //We ensure to close the reader.
-            CSVParser parser = new CSVParserBuilder().withSeparator(',').build();
-            CSVReader reader = new CSVReaderBuilder(br).withCSVParser(parser).withSkipLines(1).build();
-            List<String[]> lines = reader.readAll();
-            reader.close();
-            //Parsing lines into Item array.
-            for (String[] columns: lines) {
-                items.add(new Item(Integer.parseInt(columns[1]), columns[2], Integer.parseInt(columns[5]), Integer.parseInt(columns[7])));
-            }
-            return items;
-        }
-
-        public static String searchByName(String searchQuery, List<Item> items){
-            for (Item item: items){
-                if(item.getItemName().equalsIgnoreCase(searchQuery))
-                    return item.getItemName();
-        }
-        return null;
-    }
 }
--- src/test/java/com/revature/OSRS/AppTest.java
+++ src/test/java/com/revature/OSRS/AppTest.java
@@ -1,5 +1,7 @@
 package com.revature.OSRS;
 
+import com.opencsv.exceptions.CsvException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -13,13 +15,26 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+    static List<Item> lines;
+
+    @BeforeAll
+    public static void setup() throws IOException, CsvException {
+        lines = App.parseItems(App.loadItemCSV("OSRSItems.csv"));
+    }
     @Test
     public void givenItemName_ThenReturnItem(){
-        List<Item> lines = App.loadItems();
-
         String givenItem = "Dragon harpoon";
         String result = App.searchByName(givenItem, lines);
         assertEquals(givenItem, result);
+    }
+
+    @Test
+    public void givenNonCSVFile_ThenThrowsIllegalArgumentException(){
+        String notCSV = "OSRSItems.txt";
+        App.loadItemCSV(notCSV);
+        //Our test fails although the results are what we were expecting. Therefore we assertThrows, which we use a lambda
+        //function for.
+        assertThrows(IllegalArgumentException.class,()-> App.loadItemCSV(notCSV));
     }
 
 }
